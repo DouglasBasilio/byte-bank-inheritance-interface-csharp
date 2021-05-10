d6b4cae690058730c5ed528e6571e989ea5b2864
Douglas Basilio dos Santos <basilio.douglas@yahoo.com.br>
2021-05-10 08:02:31 -0300

    sobrecarga de métodos

---
+++ ByteBankOPP/GerenciadorBonificacao.cs
@@ -0,0 +1,28 @@
+﻿using ByteBankOPP.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankOPP
+{
+    public class GerenciadorBonificacao
+    {
+        private double _totalBonificacao;
+        public void Registrar(Funcionario funcionario)
+        {
+            _totalBonificacao += funcionario.GetBonificacao();
+        }
+
+        public void Registrar(Diretor diretor)
+        {
+            _totalBonificacao += diretor.GetBonificacao();
+        }
+
+        public double GetTotalBonificacao()
+        {
+            return _totalBonificacao;
+        }
+    }
+}
--- ByteBankOPP/Program.cs
+++ ByteBankOPP/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ByteBankOPP.Funcionarios;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,31 @@ namespace ByteBankOPP
     {
         static void Main(string[] args)
         {
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+
+            Funcionario douglas = new Funcionario();
+
+            douglas.Nome = "Douglas";
+            douglas.CPF = "123.123.123-54";
+            douglas.Salario = 2000;
+            gerenciador.Registrar(douglas);
+
+            Diretor felipe = new Diretor();
+
+            felipe.Nome = "Felipe";
+            felipe.CPF = "312.312.312-43";
+            felipe.Salario = 5000;
+            gerenciador.Registrar(felipe);
+
+            Console.WriteLine(douglas.Nome);
+            Console.WriteLine(douglas.GetBonificacao());
+
+            Console.WriteLine(felipe.Nome);
+            Console.WriteLine(felipe.GetBonificacao());
+
+            Console.WriteLine("Total de bonificações: " + gerenciador.GetTotalBonificacao());
+
+            Console.ReadLine();
         }
     }
 }
